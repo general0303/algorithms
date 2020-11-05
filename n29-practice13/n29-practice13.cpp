ddf88047147667a4184af3a6e0887f4db8bb9ade
User <psh030302@gmail.com>
2020-11-05 09:20:32 +0300

    practice 13

---
+++ n29-practice13/n29-practice13.cpp
@@ -0,0 +1,102 @@
+﻿// n29-practice13.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 8
+int main()
+{
+    int a[SIZE][SIZE]; 
+    int d[SIZE]; 
+    int v[SIZE]; 
+    int temp, minindex, min;
+    int begin_index = 0;
+    system("chcp 1251");
+    system("cls");
+    for (int i = 0; i < SIZE; i++)
+    {
+        a[i][i] = 0;
+        for (int j = i + 1; j < SIZE; j++) {
+            printf("Введите расстояние %d - %d: ", i + 1, j + 1);
+            scanf("%d", &temp);
+            a[i][j] = temp;
+            a[j][i] = temp;
+        }
+    }
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+            printf("%5d ", a[i][j]);
+        printf("\n");
+    }
+    for (int i = 0; i < SIZE; i++)
+    {
+        d[i] = 10000;
+        v[i] = 1;
+    }
+    d[begin_index] = 0;
+    do {
+        minindex = 10000;
+        min = 10000;
+        for (int i = 0; i < SIZE; i++)
+        { 
+            if ((v[i] == 1) && (d[i] < min))
+            { 
+                min = d[i];
+                minindex = i;
+            }
+        }
+        if (minindex != 10000)
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                if (a[minindex][i] > 0)
+                {
+                    temp = min + a[minindex][i];
+                    if (temp < d[i])
+                    {
+                        d[i] = temp;
+                    }
+                }
+            }
+            v[minindex] = 0;
+        }
+    } while (minindex < 10000);
+    printf("\nКратчайшие расстояния до вершин: \n");
+    for (int i = 0; i < SIZE; i++)
+        printf("%5d ", d[i]);
+    int ver[SIZE];
+    int end = 4;
+    ver[0] = end + 1;
+    int k = 1;
+    int weight = d[end]; 
+    while (end != begin_index) 
+    {
+        for (int i = 0; i < SIZE; i++)
+            if (a[i][end] != 0)
+            {
+                int temp = weight - a[i][end];
+                if (temp == d[i])
+                { 
+                    weight = temp;
+                    end = i;
+                    ver[k] = i + 1;
+                    k++;
+                }
+            }
+    }
+    getchar(); getchar();
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
